dff146f3fc1e5f9487741583e6b90e05f4283ebb
borja-x13 <bxaire13@gmail.com>
2023-04-20 11:17:09 -0700

    completed up to the story 4. Ready to move into stretch goals

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -4,4 +4,27 @@ class BlogController < ApplicationController
         @blogs = Blog.all 
     end
 
+    def show 
+        @blog = Blog.find(params[:id])
+        #it is going to look for a blog by :id
+    end
+
+    def new 
+        @blog = Blog.new
+    end
+
+    def create 
+        @blog = Blog.create(blog_params)
+        if @blog.valid?
+            redirect_to blogs_path
+        end
+    end
+
+
+    
+    private
+    # strong params # all the secret sauce
+    def blog_params
+        params.require(:blog).permit(:title, :content)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get 'blogs' => 'blog#index', as: 'blogs'
-
+  get 'blogs/new' => 'blog#new', as: 'new_blog'
   get 'blogs/:id' => 'blog#show', as: 'blog'
+  post 'blogs' => 'blog#create'
+
   root 'blog#index'
 end
 
