64401116553939953a623ea6850ab061fd0c8143
borja-x13 <bxaire13@gmail.com>
2023-04-20 11:59:34 -0700

    completed stretch goals. ready to make her pretty

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -13,6 +13,10 @@ class BlogController < ApplicationController
         @blog = Blog.new
     end
 
+    def edit
+        @blog = Blog.find(params[:id])
+    end
+
     def create 
         @blog = Blog.create(blog_params)
         if @blog.valid?
@@ -20,6 +24,20 @@ class BlogController < ApplicationController
         end
     end
 
+    def update
+        @blog = Blog.find(params[:id])
+        @blog.update(blog_params)
+        if @blog.valid?
+            redirect_to blogs_path
+        end
+    end
+
+    def destroy
+        @blog = Blog.find(params[:id])
+        if @blog.destroy
+            redirect_to blogs_path
+        end
+    end
 
     
     private
